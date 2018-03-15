b63d80e7a9b224a272902e8fdc306475314ed153
bmac <bmac325@GMail.com>
2018-03-15 12:26:36 -0400

    Add index route

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,8 +1,6 @@
 import DS from 'ember-data';
 import Ember from 'ember';
-
-var underscore = Ember.String.underscore;
-
+import { underscore } from '@ember/string';
 
 export default DS.JSONAPISerializer.extend({
   keyForAttribute: function(attr) {
